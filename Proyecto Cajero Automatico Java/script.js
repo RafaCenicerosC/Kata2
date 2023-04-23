6ba6172731fd382cff09cbe7f93b5f0f7d5dd728
Rafael Ceniceros <rrafa7114@gmail.com>
2023-04-22 21:49:16 -0600

    Pagina inicio sesion

---
+++ Proyecto Cajero Automatico Java/script.js
@@ -0,0 +1,30 @@
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const button = document.getElementById('button');
+const alertDiv = document.getElementById('alert');
+
+const users = [
+  { username: 'user1', password: 'pass1' },
+  { username: 'user2', password: 'pass2' },
+  { username: 'user3', password: 'pass3' },
+];
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const data = {
+    username: username.value,
+    password: password.value,
+  };
+
+  // Check if username and password match any user
+  const user = users.find((u) => u.username === data.username && u.password === data.password);
+
+  if (user) {
+    // Redirect to new page
+    window.location.href = "bankapp.html";
+  } else {
+    // Show alert
+    alertDiv.innerHTML = 'Username or password is incorrect.';
+    alertDiv.classList.add('alert');
+  }
+});
