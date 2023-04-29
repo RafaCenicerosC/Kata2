2d66afad9f61145d06ea3ec444450f47d1397e8d
Rafael Ceniceros <rrafa7114@gmail.com>
2023-04-29 00:21:43 -0600

    Diseño Inicio listo

--- Proyecto Cajero Automatico Java/script2.js
+++ Proyecto Cajero Automatico Java/script2.js
@@ -59,3 +59,17 @@ const bloques = document.querySelectorAll('.bloque')
          closebutton2.addEventListener('click', () => {
          withdrawwindow.close();
          });
+
+         const tarjeta = document.querySelector('#tarjeta');
+	
+         // * Volteamos la tarjeta para mostrar el frente.
+         const mostrarFrente = () => {
+             if(tarjeta.classList.contains('active')){
+                 tarjeta.classList.remove('active');
+             }
+         }
+         
+         // * Rotacion de la tarjeta
+         tarjeta.addEventListener('click', () => {
+             tarjeta.classList.toggle('active');
+         });
